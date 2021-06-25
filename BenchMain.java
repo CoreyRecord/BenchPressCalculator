d3eb4f00a0a68bc91686b092f33c24eb8a501a3b
CoreyRecord <73188618+CoreyRecord@users.noreply.github.com>
2021-06-24 22:12:29 -0400

    Add files via upload

---
+++ BenchMain.java
@@ -0,0 +1,160 @@
+// Corey Record
+
+// This program will be a bench press calculator, that will help you
+// to improve your 1 rep max on bench press
+
+// A variable in programming is a storage location that is paired with a name
+// this variable contains information referred to as a value or data
+
+// Scope is where an item with a name is recognized
+// Scope is an association of a name to an entity like a variable
+
+// Casting is when you assign a value to a data type to a different data type
+// like int to double
+
+// Data Types
+// boolean are true and false only primitive data type
+// byte is a designate 8 bit signed integer primitive data type
+// char defines a charter as a primitive data type
+// short is a 16 bit signed integer primitive data type
+// int is a 32 bit signed integer primitive data type
+// long is a 64 bit signed integer primitive data type
+// float is a 32 bit float primitive data type that allows decimal
+// double is a 64 bit float primitive data type that needs to be store
+
+// Inheritance is reusing code and not having to make more classes
+// This means that we dont have to waste time making repeptive classes
+
+// Polymorphism is an object taking on many forms
+http:// marketplace.eclipse.org/marketplace-client-intro?mpc_install=150
+
+package BenchPress;
+
+import java.util.Scanner;
+import java.util.Random;
+
+/**
+ * A driver class that calculates a program that can help improve your benchpress.
+ * 
+ * @author Corey Record
+ *
+ */
+public class BenchMain {
+
+  public static void main(String[] args) {
+	
+					
+				System.out.println("Welcome to the Bench Press Calculator!");
+				System.out.println("Is it true that you want to improve your bench press?");
+				Scanner sc = new Scanner(System.in);
+				boolean bn = sc.nextBoolean();
+				if (bn == true) {
+					System.out.println("So you want to improve your bench press");
+				}else if (bn == false) {
+					System.out.println("So you dont want to improve your bench press");
+				}
+				sc.close();
+				
+				int benchPressMax; // variable declaration
+				benchPressMax = 225;
+				final double bodyWeight = 198.64;
+				System.out.print(Math.floor(bodyWeight));
+				
+				int bwInt = (int) bodyWeight; // Manual Casting
+				String s1 = new String("userHeight");
+				
+				int benchMax = 1;
+				
+				switch(benchMax) {
+					case 1:
+						System.out.println("Beginner");
+						break;
+					case 2:
+						System.out.println("Intermediate");
+						break;
+					case 3:
+						System.out.println("Advanced");
+						break;
+					case 4:
+						System.out.println("Elite");
+						break;
+				String skill = benchMax > 3 ? "The lifter is skilled";
+				int squatMax = 2;
+				
+				int deadliftMax = 3;
+				
+				//method call
+				total(1, 2, 3);// arguments squat bench and deadlift max
+				}
+				//escape sequence
+				System.out.print("Your \"bodyWeight\" is good");
+			
+						// a class is like a blueprint
+						// an object is an instance of a class
+						// an object is an instance of a class
+						// an object is very similar to a variable
+						// a class is very similar to a data type
+						//Dog dixie;
+						//dixie.age = 2;
+						//dixie.breed = "Lab";
+						//dixie.color = "Chocolate";
+						//dixie.bark();
+						//System.out.println("My Dog rogram")
+				}
+
+  public static int max(int max1,int max2) {
+					int result;
+					if (max1> max2);
+						result = max1
+					else; 
+						result = max2;
+					
+					return result;
+					System.out.println("Max is greater than Previous");
+				
+				}
+
+  // header and parameters
+  public static void total(int benchMax, int squatMax,
+      int deadliftMax);System.out.println(benchMax+squatMax+deadliftMax);
+
+  }
+
+  public static void random(String[] args) {
+    Random barbell = new Random();
+    int plates;
+
+    for (int counter = 1; counter <= 10; coutner++) {
+      plates = 1 + barbell.nextInt(6);
+      System.out.println(plates + " ");
+    }
+  }
+
+  public static void strings(String[] args) {
+    String a = "Barbell Squat";
+    String b = "Barbell Squat";
+
+    // == works as it checks to see if they are identical or not
+    if (a == b) {
+      System.out.println("true");
+    } else {
+      System.out.println("False");
+
+    }
+    class benchArray {
+      public static void main(String args[]);
+
+      {
+        String[] x = {"135", "185", "225"};
+
+        args = x;
+        for (String i : x) {
+          System.out.println(i);
+
+        }
+      }
+    }
+  }
+}
+
+
